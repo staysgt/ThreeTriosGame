168df4c93b76dbf90868a65524cef1825cddca72
Sarah Taylor <150694563+staysgt@users.noreply.github.com>
2024-11-13 15:28:42 -0500

    nov 13

--- src/cs3500/view/Graphics2DInf.java
+++ src/cs3500/view/Graphics2DInf.java
@@ -7,6 +7,18 @@ import java.awt.*;
  */
 public interface Graphics2DInf {
 
+  /**
+   * This allows the rectangle to be put on the grid.
+   *
+   * @param g2d    2D graphics
+   * @param x      coordinate x
+   * @param y      coordinate y
+   * @param width  width of rectangle
+   * @param height height of rectangle
+   * @param row    rows
+   * @param col    columns
+   * @param isHand if it is in users hand
+   */
   void fillRect(Graphics2D g2d, int x, int y, int width, int height, int row, int col,
                 boolean isHand);
 
